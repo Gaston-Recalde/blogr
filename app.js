fd2b067b6e851052f2d79eb11acce9c79d45b919
Gaston Recalde <gastonrg94@gmail.com>
2022-12-06 19:41:29 -0300

    Proyecto terminado

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-// Barra de navegación responsive
 const navToggle = document.querySelector(".nav__toggle");
 const navUl = document.querySelector(".nav__ul");
-const body = document.querySelector("body");
+const ul__li = document.querySelectorAll('.ul__li');
+const ul__container = document.querySelectorAll('.ul__container');
+const flecha = document.querySelectorAll('.li__arrow');
+const flecha2 = document.querySelectorAll('.li__arrow--light');
+const open__nav = document.querySelector('.open');
+const close__nav = document.querySelector('.close');
 
 navToggle.addEventListener("click", (e) => {
   e.stopPropagation();
   navUl.classList.toggle("nav__ul__visible");
-
-  if (navUl.classList.contains("nav__ul__visible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menú");
-  } else {
-    navToggle.setAttribute("aria-label", "Abrir menú");
-  }
+  open__nav.classList.toggle('open--close');
+  close__nav.classList.toggle('close--active');
 });
 
-body.addEventListener("click", () => {
-    navUl.classList.remove("nav__ul__visible");
+ul__li.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    ul__container[index].classList.toggle('ul__container--active');
+
+    flecha[index].classList.toggle('up');
+    flecha2[index].classList.toggle('up');
+  });
 });
